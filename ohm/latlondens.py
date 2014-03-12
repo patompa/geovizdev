688fc2f146eb0c4223bf0e0d87462bb22d485cde
Thomas Sandholm <sandholm@sandholm-HP-Z400-Workstation.(none)>
2014-03-12 16:39:32 -0700

    Added Open Heat Map example

---
+++ ohm/latlondens.py
@@ -0,0 +1,23 @@
+from geohash import encode
+f=open('../sample1.tsv')
+lines = f.read().strip().split('\n')
+areas = {}
+for line in lines[1:]:
+  cols = line.strip().split('\t')
+  lat = float(cols[0])
+  lon = float(cols[1])
+  geo = encode(lat,lon)[0:3]
+  if not areas.has_key(geo):
+     areas[geo] = 0
+  areas[geo] += 1
+
+out=open('latlon.csv','w')
+out.write('lat,lon,value\n')
+for line in lines[1:]:
+  cols = line.strip().split('\t')
+  lat = cols[0]
+  lon = cols[1]
+  geo = encode(float(lat),float(lon))[0:3]
+  value = areas[geo]
+  out.write('%s,%s,%d\n' % (lat,lon,value))
+
